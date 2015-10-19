b123b63d01e66490748d565a73411b4a4ee1e2a1
maximumG <none>
2015-10-19 15:56:02 +0200

    code modification & adjustment

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 """
-piapi module stands for (Cisco) **P**rime **I**nfrastructure **API**.
+piapi module stands for (Cisco) Prime Infrastructure API.
 The module implements the PIAPI class which helps interacting with the Cisco Prime Infrastructure REST API using
 simple methods that can either request data or request an action.
 
 The Cisco Prime Infrastructure API is a REST API which exposes several resources that can be of 2 types:
-* **Data** resources: expose some data collected by the software which can be retrieved (e.g: client summary).
-* **Action** resources: expose some action that can modify the configuration of the software (e.g: modify/update an Access Point)
+    * Data resources: expose some data collected by the software which can be retrieved (e.g: client summary).
+    * Action resources: expose some action that can modify the configuration of the software (e.g: modify/update an Access Point)
 
 The REST API is applying request rate limiting to avoid server's overloading. To bypass this limitation, especially
 when requesting data resources, the PIAPI uses multithreading requests (grequests library) with an hold time between
