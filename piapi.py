4b9ea7eb3239112c35f7953edf2bada49285b333
Jimmy Taylor <11437306+consentfactory@users.noreply.github.com>
2022-04-30 08:00:14 -0600

    Updated header to match styling

--- piapi.py
+++ piapi.py
@@ -8,26 +8,36 @@
 #
 #   Unless required by applicable law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+#   implied. See the License for the specific language governing 
+#   permissions and limitations under the License.
+
 """
 piapi module stands for (Cisco) Prime Infrastructure API.
-The module implements the PIAPI class which helps interacting with the Cisco Prime Infrastructure REST API using
-simple methods that can either request data or request an action.
 
-The Cisco Prime Infrastructure API is a REST API which exposes several resources that can be of 2 types:
-    * Data resources: expose some data collected by the software which can be retrieved (e.g: client summary).
-    * Service resources: expose some services that can modify the configuration of the software (e.g: modify/update an Access Point)
+The module implements the PIAPI class which helps interacting with the 
+Cisco Prime Infrastructure REST API using simple methods that can 
+either request data or request an action.
 
-The REST API is applying request rate limiting to avoid server's overloading. To bypass this limitation, especially
-when requesting data resources, the PIAPI uses multithreading requests (grequests library) with an hold time between
-chunk of requests. Please check the documentation to knowns more about rate limiting.
+The Cisco Prime Infrastructure API is a REST API which exposes several 
+resources that can be of 2 types:
+    * Data resources: expose some data collected by the software 
+      which can be retrieved (e.g: client summary).
+    * Service resources: expose some services that can modify the 
+      configuration of the software (e.g: modify/update an Access Point)
 
-Also note that the piapi module only works with the JSON structure exposed by the REST API. The module doesn't support
-the default XML structure.
+The REST API is applying request rate limiting to avoid server's 
+overloading. To bypass this limitation, especially when requesting data 
+resources, the PIAPI uses multithreading requests (grequests library) 
+with an hold time between chunk of requests. Please check the 
+documentation to knowns more about rate limiting.
 
-Please check your Cisco Prime REST API available at http://{server-name}/webacs/api/v1/
+Also note that the piapi module only works with the JSON structure 
+exposed by the REST API. The module doesn't support the default XML 
+structure.
+
+Please check your Cisco Prime REST API available at 
+http://{server-name}/webacs/api/v1/
 """
 
 import requests
